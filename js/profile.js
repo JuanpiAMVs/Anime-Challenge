39f0c4d0a101ec9cda77edde99922c8e290fabd1
Juan Pablo Godoy <juenpablogodoyn@gmail.com>
2022-03-26 18:53:07 -0300

    Cambio de contraseña

--- js/profile.js
+++ js/profile.js
@@ -5,4 +5,18 @@ search.appendChild(user)
 user.className = "user"
 
 const userNameJson = JSON.parse(sessionStorage.getItem('email'))
-user.innerHTML = userNameJson.email[0].toUpperCase()
+const {clave:contraseña} = userNameJson
+console.log(contraseña)
+user.innerHTML = userNameJson.email[0].toUpperCase();
+
+
+function changePassword(newP, numberUser){
+    const newPassword = {
+        ...usuarios[numberUser],
+        clave: newP,
+        
+    }
+    console.log(newPassword)
+}
+
+changePassword("mondongo", 0)
